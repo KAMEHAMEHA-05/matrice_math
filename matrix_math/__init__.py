62696ca6b8aeec9418f099defe852492c9f4671c
KAMEHAMEHA-05 <87436897+KAMEHAMEHA-05@users.noreply.github.com>
2022-08-10 17:42:25 +0530

    Add files via upload

---
+++ matrix_math/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from matrix_math import add
+from matrix_math import subtract
+from matrix_math import scalar_mult
+from matrix_math import matrix_mult
+from matrix_math import minor
+from matrix_math import transpose
+from matrix_math import cofactor
+from matrix_math import minor_mat
+from matrix_math import cofactor_mat
+from matrix_math import adjoint
+from matrix_math import determinant
+from matrix_math import inverse_mat
+
