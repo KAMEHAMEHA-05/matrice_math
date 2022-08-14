c75cd832c965dc033c1a4fc028fc35e127783064
KAMEHAMEHA-05 <87436897+KAMEHAMEHA-05@users.noreply.github.com>
2022-08-14 20:10:51 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from distutils.core import setup
 setup(
   name = 'matrice_math',         # How you named your package folder (MyLib)
   packages = ['matrice_math'],   # Chose the same as "name"
-  version = '1.1.1',      # Start with a small number and increase it with every change you make
+  version = '1.1.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Matrice_math is package that can be used for matrix and determinant computing with Python',   # Give a short description about your library
   author = 'Ishaan Sawant',                   # Type in your name
